8dfd4c8c036c8e4ee53b9ad2fc331db35b664c9a
Quang Vu <ngqangvvu@gmail.com>
2022-12-24 18:34:34 +0700

    Update product description

--- src/components/products/productDesc.jsx
+++ src/components/products/productDesc.jsx
@@ -12,22 +12,17 @@ Properties:
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
-
 import { Button } from '../buttons'
 
 function ProductDesc({
   name,
   price,
   desc,
-  width,
-  height,
-  depth,
   quantity,
   saved,
   handleAddToCart,
   handleAddToWishlist,
-  removeFromWishlist,
-  handleComment
+  removeFromWishlist
 }) {
   //
   const [count, setCount] = useState(1)
@@ -45,45 +40,17 @@ function ProductDesc({
       {/* Top Text */}
       <div className="mt-7 mb-4 laptop:mt-9 laptop:mb-7">
         <h3 className="text-h3 mb-3 laptop:text-h1 laptop:mb-4">{name}</h3>
-        <h4 className="text-h4 laptop:text-h3">{price} VND</h4>
+        <h4 className="text-h4 laptop:text-h3">{price}</h4>
       </div>
       <hr />
       {/* Description */}
       <div className="mt-7 laptop:mt-6">
-        <h5 className="text-h5 mb-3">Product description</h5>
-        <small className="text-body-sm laptop:text-body-md">{desc}</small>
-      </div>
-      {/* Dimensions */}
-      <div className="mt-7 laptop:mt-9 laptop:w-80">
-        <h5 className="text-h5 mb-3 laptop:mb-6">Dimensions</h5>
-        <hr className="hidden" />
-        {/* Dimensions Metrics */}
-        <div className="flex justify-between items-center mt-4 ">
-          <div>
-            <h6 className="text-h6 mb-4">Height</h6>
-            <small className="text-body-sm laptop:text-body-md">
-              {height}cm
-            </small>
-          </div>
-          <div className="h-11 w-1 bg-border_grey"></div>
-          <div>
-            <h6 className="text-h6 mb-4">Width</h6>
-            <small className="text-body-sm laptop:text-body-md">
-              {width}cm
-            </small>
-          </div>
-          <div className="h-11 w-1 bg-border_grey"></div>
-          <div>
-            <h6 className="text-h6 mb-4">Depth</h6>
-            <small className="text-body-sm laptop:text-body-md">
-              {depth}cm
-            </small>
-          </div>
-        </div>
+        <strong className="text-h5 mb-5">Product description</strong> <br />
+        <p className="text-body-sm laptop:text-body-md">{desc}</p>
       </div>
       {/* Stepper */}
       <div className="mt-7 laptop:w-fit laptop:mt-10">
-        <h5 className="text-h5 mb-3 ">Quantity</h5>
+        <strong className="text-h5 mb-5 ">Quantity</strong>
         <div className="flex items-center gap-2">
           {/*Counter*/}
           <div>
@@ -108,12 +75,6 @@ function ProductDesc({
           {/*remain on stock*/}
           <div>{quantity} items</div>
           {/**/}
-          <div
-            className="cursor-pointer w-fit px-2 bg-primary rounded-full text-light_grey"
-            onClick={handleComment}
-          >
-            Comment
-          </div>
         </div>
       </div>
       {/* Group btn */}
